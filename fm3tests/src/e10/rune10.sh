27b60876abe65069cb6f780d946255436330797d
Rodney Bates <rodney.m.bates@acm.org>
2025-05-28 20:20:28 -0500

    Entire e10 batch, passing & updated.  Scripts updated too.

---
+++ fm3tests/src/e10/rune10.sh
@@ -0,0 +1,23 @@
+#/usr/bin/bash
+
+TESTSDIR="."
+
+function runtest # $1 is test directory simple name, e.g. e1001.
+  {
+    ../onetest.sh compile $1 
+    ../onetest.sh check $1 
+  }
+
+runtest e1001 
+runtest e1002 
+runtest e1003 
+runtest e1004 
+runtest e1005 
+runtest e1006 
+runtest e1007 
+runtest e1008 
+runtest e1009 
+runtest e1010 
+runtest e1011 
+
+
--- fm3tests/src/onetest.sh
+++ fm3tests/src/onetest.sh
@@ -7,13 +7,20 @@
 # compile runs a single compile that incldes the  entire test case.
 # check checks output files for presence and expected results.
 # test does compile followed by check.
-# add does a git adds for everything to be tracked in the test case.
 # update makes each *.expected file equal to its corresponding output file.
-# $2 is the top directory for the test, named, for example e1011. If omitted,
-# defaults to "."
+# add does a git adds for everything to be tracked in the test case.
+
+# $2 is the top directory for the test, named, for example e1011. If omitted, defaults to "."
 
 ACTION=$1
 ESALL=0
+
+# Text attributes:
+# Why are these different from codes in FM3TextColors.i3? 
+RED="\\033[31m"
+GREEN="\\033[32m"
+ORANGE="\\033[33m"
+PLAIN="\\033[0m"
 
 # Variables herein that are semantically boolean follow the bash exit status
 # convention that 0 is true and 1 is false. 
@@ -35,44 +42,52 @@ ESALL=0
       elif [ ${ACTION}% = update% ]
       then doupdate=0
       else
-        echo $TAG Unknown action: ${ACTION}
-        echo $TAG "  Must be one of \"compile\", \"check\", \"test\", \"add\", or \"update\""
+        echo -e $TAG Unknown action: ${ACTION}
+        echo -e $TAG "    Must be one of \"compile\", \"check\", \"test\", \"add\", or \"update\""
         ESALL=1
         return 1  
       fi
     }
 
-    function compile
+  function multilog # $1 is messsge
+    {
+      if [ $docompile = 0 ]; then echo -e "$1" | tee -a $TESTLOG; fi
+      if [ $docheck = 0 ];   then echo -e "$1" | tee -a $CHECKLOG; fi
+      if [ $doupdate = 0 ];  then echo -e "$1" | tee -a $UPDATELOG; fi
+      if [ $doadd = 0 ];     then echo -e "$1" | tee -a $ADDLOG; fi
+    } 
+
+  function compile
     { # $SOURCES is list of source file simple names for the compile command line.
       local FM3
       local CLARGS
       local ES
-      if [ $ESALL -ne 0 ]; then return; fi
       if [ $docompile = 0 ] 
       then
-        # Optional file named fm3 conatains path to compiler executable. 
+
+        # Optional file named fm3 contains path to compiler executable. 
         if test -f fm3 ; then FM3=`cat fm3`; else FM3="../../FM3"; fi
         FM3=`abspath $FM3`
         # Optional file named clargs contains compile command line options.
         if test -f clargs
-        then CLARGS=`cat clargs`
+        then CLARGS=`/clargs`
         else CLARGS="--disasm --no-disasm-verbose --exprs --no-expr-addrs" 
         fi 
-        echo $TAG `$FM3 --version 2>&1` | tee -a $TESTLOG 
+        echo -e `$FM3 --version 2>&1` | tee -a $TESTLOG 
         CMD="$FM3 $CLARGS $SOURCES"
-        echo $TAG "Running compile in $SRCDIR," | tee -a $TESTLOG 
-        echo $TAG "  using command $CMD" | tee -a $TESTLOG 
+        echo -e $TAG "Compiling in $SRCDIR," | tee -a $TESTLOG 
+        echo -e "$TAG   using command \"$CMD\"" | tee -a $TESTLOG 
         cd $SRCDIR
         $CMD 2>&1 | tee tmplog0 
         ES=$?
         cat tmplog0 >> $TESTLOG
-        # rm tmplog0
+        rm tmplog0
         if [ $ES -ne 0 ]
         then #compiler failure 
           echo -e $TAG $RED "Compiler failed." $PLAIN  
           ESALL=1  
         else #Compiler run ended normally.
-          echo -e $TAG $GREEN "Compile finished OK" $PLAIN 
+          echo -e $TAG $GREEN "Compile finished OK" $PLAIN | tee -a $TESTLOG  
         fi
       fi
     }
@@ -81,14 +96,24 @@ ESALL=0
     { # Do a git add for a tracked file.
       # $1 is directory containing the file.
       # $2 is file simple name.
-      local ES 
+      local ES
+      local CMD 
       if [ $doadd = 0 ]  
       then       
         if test -f ${1}/${2}
-        then
-          git add ${1}/${2} 
+        then # File exists. 
+          CMD="git add ${1}/${2}"
+          #multilog "$TAG In directory ${1}," 
+          echo -e -n "$TAG Running  $CMD ..." | tee  -a $ADDLOG 
+          $CMD 2>&1 | tee -a $ADDLOG
           ES=$?
-          if [ ES > 0 ]; then ESALL=1; fi
+          if [ $ES = 0 ]
+          then 
+            echo -e $GREEN "OK." $PLAIN | tee -a $ADDLOG
+          else
+            echo -e $RED "FAILED." $PLAIN | tee -a $ADDLOG
+            ESALL=1
+          fi
         fi
       fi
     } 
@@ -98,13 +123,23 @@ ESALL=0
       # $1 is directory containing the file and its expected file ${2}.expected
       # $2 is file simple name.
       local ES 
+      local CMD 
       if [ $doupdate = 0 ] 
       then
         if test -f ${1}/${2}
-        then
-          cp -p ${1}/${2} ${1}/${2}.expected
+        then # File exists. 
+          CMD="cp -p ${1}/${2} ${1}/${2}.expected"
+          #multilog "$TAG In directory ${1}," 
+          echo -e -n "$TAG Running  $CMD ..." | tee  -a $UPDATELOG 
+          $CMD 2>&1 | tee -a $UPDATELOG # Produces no output? 
           ES=$?
-          if [ ES > 0 ]; then ESALL=1; fi
+          if [ $ES = 0 ]
+          then 
+            echo -e $GREEN "OK." $PLAIN | tee -a $UPDATELOG
+          else
+            echo -e $RED "FAILED." $PLAIN | tee -a $UPDATELOG
+            ESALL=1
+          fi
         fi
       fi
     } 
@@ -126,58 +161,72 @@ ESALL=0
           DIFFCMD="diff -u ${2}.expected ${2}"
           PREVDIR=`pwd`
           cd ${1}
-          echo $TAG "In directory ${1}," | tee -a $TESTLOG
-          echo $TAG "  running" $DIFFCMD | tee -a $TESTLOG 
+          #multilog "$TAG In directory ${1}," 
+          echo -e -n "$TAG Running $DIFFCMD ..." | tee -a $CHECKLOG 
           $DIFFCMD 2>&1 > $DIFFLOG  
           ES=$?
           if [ ${ES} = 0 ] # No differences.
           then
             rm -f ${DIFFLOG}
             rm -f ${TESTDIR}/${DIFFLOG} # Possible old version.
-            echo -e $TAG $GREEN " OK." $PLAIN 
+            echo -e $GREEN "OK." $PLAIN | tee -a $CHECKLOG
           else
             mv $DIFFLOG $TESTDIR
-            echo -e $TAG $RED " File" ${2} not as expected $PLAIN | tee -a $TESTLOG
+            echo -e "${RED} Not as expected$PLAIN" | tee -a $CHECKLOG
             ESALL=1
           fi
           cd $PREVDIR
         #elif test -f ${1}/${2}.expected 
         #then #expected file exists (but is empty).  Ignore this case.
-        #  echo $TAG Expected file ${1}/${2}.expected "is empty, ignored." 2>&1 | tee -a $TESTLOG
+        #  echo -e $TAG Expected file ${1}/${2}.expected "is empty, ignored." 2>&1 | tee -a $CHECKLOG
         else #expected file is missing altogether. 
-          echo $TAG Expected file ${1}/${2}.expected is missing. 2>&1 | tee -a $TESTLOG
+          echo -e "$TAG Expected file ${1}/${2}.expected is ${RED}missing${PLAIN}." | tee -a $CHECKLOG
           ESALL=1
         fi
       fi
     }
 
+# Main body.    
 STARTDIR=`pwd` # To return to at end.
 if [ "${2}%" = "%" ] ; then DIR="."; else DIR="${2}"; fi
 TESTDIR="`abspath $DIR`"
-TESTLOG="${TESTDIR}/tmptestlog" # Will rename at end.
-rm -f $TESTLOG # Just in case. 
-rm -f ${TESTDIR}/SUCCEEDED ${TESTDIR}/FAILED ${TESTDIR}/NOTRUN
-echo $TAG "Test directory is $TESTDIR" | tee -a $TESTLOG
+cd $TESTDIR
 
 # Does $TESTDIR/run.sh contain a unique test script for this test. 
-cd $TESTDIR
 if test -f run.sh 
 then # run it with same arg.
   ./run.sh $ACTION 
 else # Do this script.
-  TAG="script: "
-  checkarg  
+
+  TAG="${ORANGE}script:$PLAIN "
+  TAGN="          " 
+  checkarg
+
+  if test -f donotrun;
+  then
+    echo -e "${TAG} ${GREEN}Not running in ${TESTDIR}${PLAIN}" 
+    touch ${TESTDIR}/NOTRUN
+    exit
+  fi      
+
+  TESTLOG="${TESTDIR}/tmptestlog" # Will rename at end.
+  # Leave the leftover one around during compile.
+  CHECKLOG="${TESTDIR}/checklog"
+  if [ $docheck = 0 ]; then rm -f $CHECKLOG; fi # Remove old leftover.
+  UPDATELOG="${TESTDIR}/updatelog"
+  if [ $doupdate = 0 ]; then rm -f $UPDATELOG; fi  # Remove old leftover.
+  ADDLOG="${TESTDIR}/addlog"
+  if [ $doadd = 0 ]; then rm -f $ADDLOG; fi # Remove old leftover.
+  if [ $docheck = 0 ]
+  then rm -f ${TESTDIR}/SUCCEEDED ${TESTDIR}/FAILED ${TESTDIR}/NOTRUN
+  fi 
+  multilog "$TAG Initial directory = $STARTDIR"
+  multilog "$TAG Test directory is $TESTDIR" 
+    
   if [ $ESALL -ne 0 ]; then exit 1; fi
 
   SRCDIR="$TESTDIR/src"
   BUILDDIR="$TESTDIR/build"
-
-  # Text attributes:
-  # Why are these different from codes in FM3TextColors.i3? 
-  RED="\\033[31m"
-  GREEN="\\033[32m"
-  ORANGE="\\033[33m"
-  PLAIN="\\033[0m"
 
   # Optional file srcdirs lists directories to look for source files in.
   if test -f srcdirs ; then SRCDIRS=`cat srcdirs`; else SRCDIRS="$SRCDIR"; fi
@@ -191,12 +240,12 @@ else # Do this script.
   if test -f imports ; then IMPORTS=`cat imports`; else IMPORTS=""; fi
   
   ALLSRCS="$SOURCES $IMPORTS"
+  echo SOURCES = $SOURCES
+  echo IMPORTS = $IMPORTS
+  echo ALLSRCS = $ALLSRCS
 
   # Optional file streams contains name suffixes of intermediate stream files.
-  if test -f streams
-  then STREAMS=`cat streams`
-  else STREAMS="FM3Pass1 FM3Pass2"
-  fi
+  if test -f streams; then STREAMS=`streams`; else STREAMS="FM3Pass1 FM3Pass2"; fi
 
   # Optional file exprs contains name suffixes of data structure dumps files.
   if test -f exprs ; then EXPRS=`cat exprs`; else EXPRS="FM3Pass2.Exprs"; fi
@@ -205,10 +254,19 @@ else # Do this script.
   compile
 
   # FM3Log
-    checkOneFile ${SRCDIR} FM3Log
-    updateOneFile ${SRCDIR} FM3Log
-    addOneFile ${SRCDIR} FM3Log
-    addOneFile ${SRCDIR} FM3Log.expected
+  checkOneFile ${SRCDIR} FM3Log
+  updateOneFile ${SRCDIR} FM3Log
+  addOneFile ${SRCDIR} FM3Log
+  addOneFile ${SRCDIR} FM3Log.expected
+
+  # Test environment files:
+  addOneFile ${SRCDIR} srcdirs
+  addOneFile ${SRCDIR} sources
+  addOneFile ${SRCDIR} imports
+  addOneFile ${SRCDIR} streamss
+  addOneFile ${SRCDIR} exprs
+  # addOneFile ${SRCDIR} donotrun
+  # Weird paradox. Will have to be added and removed manually.  
     
   # Command line source files:
   for SRC in $SOURCES
@@ -227,6 +285,7 @@ else # Do this script.
     # Per-source log files: 
     checkOneFile ${SRCDIR} ${SRC}.Log
     updateOneFile ${SRCDIR} ${SRC}.Log
+    addOneFile ${SRCDIR} ${SRC}
     addOneFile ${SRCDIR} ${SRC}.Log
     addOneFile ${SRCDIR} ${SRC}.Log.expected
   done
@@ -253,20 +312,21 @@ else # Do this script.
   done # Sources
 
   # Summarize:
-  echo $TAG $STARTDIR
-  cd $STARTDIR
-  echo $TAG "Last action was" $ACTION '.' | tee -a $TESTLOG
-  if [ $docheck = 0 ] && [ $ESALL -ne 0 ] 
-  then # Overall failure
-    touch ${TESTDIR}/FAILED 
-    echo -e $TAG $RED "################ F A I L E D ################" $PLAIN | tee -a $TESTLOG
-    echo $TAG "---------------- Failed in `pwd`" | tee -a $TESTLOG
-    mv $TESTLOG $TESTDIR/testlog #Overlaying the old one. 
-    exit 1
-  else # Overall success
-    touch ${TESTDIR}/SUCCEEDED
-    touch ${TESTDIR}/LASTSUCCEEDED
-    echo -e $TAG $GREEN "---------------- Succeeded in `pwd`" $PLAIN | tee -a $TESTLOG
-    mv $TESTLOG $TESTDIR/testlog #Overlaying the old one. 
+  if [ $docheck = 0 ] 
+  then # Doing check
+    if [ $ESALL -ne 0 ]
+    then # Check failure 
+      touch ${TESTDIR}/FAILED 
+      echo -e -n $TAG $RED "################ F A I L E D ################" $PLAIN | tee -a $CHECKLOG
+      echo -e " check(s)  in `pwd`" | tee -a $CHECKLOG
+    else # Check success.
+      touch ${TESTDIR}/SUCCEEDED
+      touch ${TESTDIR}/LASTSUCCEEDED
+      echo -e "$TAG $GREEN ---------------- Succeeded in `pwd` $PLAIN" | tee -a $CHECKLOG 
+    fi
   fi
+  if [ $docompile = 0 ]; then mv $TESTLOG $TESTDIR/testlog; fi #Overlaying the old one.
+  cd $STARTDIR
+  if [ $ESALL -ne 0 ]; then exit 1; fi 
+
 fi 
